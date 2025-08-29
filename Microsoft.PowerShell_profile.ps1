6c9b4a8cfd6c938acbf597a69788a9c3f0976ad6
MrHencke <henckedev@outlook.com>
2025-08-29 10:43:23 +0200

    Add rustup completions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,10 +2,10 @@
 . "$PSScriptRoot\init.ps1"
 . "$PSScriptRoot\zoxide.ps1"
 . "$PSScriptRoot\custom-functions.ps1"
+. "$PSScriptRoot\rustup-completions.ps1"
 
 # FNM
 fnm env --use-on-cd | Out-String | Invoke-Expression
-
 
 # Starship
 Invoke-Expression (&'C:\Program Files\starship\bin\starship.exe' init powershell)
@@ -18,3 +18,7 @@ Set-PSReadLineOption -EditMode Windows
 #Fzf
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
